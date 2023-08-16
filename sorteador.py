901cba09760073e4516f2b99728c2a1dd25e8799
Amber Forte <111364597+ambercff@users.noreply.github.com>
2023-08-16 10:24:19 -0300

    Update sorteador.py

--- sorteador.py
+++ sorteador.py
@@ -1,30 +1,37 @@
 import random
+import time
 
 vida = 200
 
-num_sorteado = random.randint(0,100)
+num_sorteado = random.randint(0,11)
 
-num_par = num_sorteado % 2
+lista = [0,1,1,2,3,5,8,13,21,34,55,89]
+num_lista = lista[num_sorteado]
 
-print(num_sorteado)
+count = 1
 
-if num_par != 0:
-    num_sorteado = random.randint(0,100)
+tempo_total = 60
     
-else:
-    for i in range(4,0,-1):
-        num_usuario = int(input("digite um número de 0 a 100: "))
+for i in range(4,0,-1):
+    print(f"restam {round(tempo_total, 2)} segundos")
+    antes = time.time()
+    num_usuario = int(input("digite um número de 0 a 100: "))
+    tempo_total -= time.time() - antes
 
-        if num_sorteado == num_usuario:
-            print(f"Parabéns! Você ganhou.")
+    if tempo_total <= 0:
+        print("Seu tempo acabou")
+        break
+
+    if num_lista == num_usuario:
+        print(f"Parabéns! Você ganhou.")
+        break
+    else:
+        dano = abs((num_lista - num_usuario))
+
+        if dano >= vida or i<= 1:
+            print(f"Você perdeu! o número era {num_lista}")
             break
-        else:
-            dano = abs((num_sorteado - num_usuario))
 
-            if dano >= vida or i<= 1:
-                print(f"Você perdeu! o número era {num_sorteado}")
-                break
+        vida -= dano
 
-            vida -= dano
-
-            print(f"Você errou. Restam {vida} pontos de vida e {i - 1} tentativas")
+        print(f"Você errou. Restam {vida} pontos de vida e {i - 1} tentativas")
